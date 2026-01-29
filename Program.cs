79551d98a2cd013b9ccb4541af08421a985de7cd
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-28 20:30:37 -0800

    Section A was done and works good

--- Program.cs
+++ Program.cs
@@ -5,7 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("SECTION A - Academic Term Identification");
-            Console.WriteLine("SECTION B - Campus Access Check");
+            int currentMonth = int.Parse(Console.ReadLine());
+
+            if (currentMonth >= 1 && currentMonth <= 5)
+            {
+                Console.WriteLine("Spring Term");
+            }
+            else if (currentMonth >= 6 && currentMonth <= 7)
+            {
+                Console.WriteLine("Summer term");
+            }
+            else
+            {
+                Console.WriteLine("Fall Term");
+            }
+
+            Console.WriteLine("\nSECTION B - Campus Access Check");
+
             Console.WriteLine("SECTION C - Semester Code Lookup");
 
         }
