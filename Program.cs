49e56753e168b6b5283bffbedb4984c9cc19f6bd
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-30 22:10:49 -0800

    Section c was done

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("SECTION A - Academic Term Identification");
-            Console.WriteLine("Current Month: ");
+            Console.Write("Current Month: ");
             int currentMonth = int.Parse(Console.ReadLine());
 
             if (currentMonth >= 1 && currentMonth <= 5)
@@ -22,19 +22,45 @@
             }
 
             Console.WriteLine("\nSECTION B - Campus Access Check");
-            Console.WriteLine("Enter your age: ");
+            Console.Write("Enter your age: ");
             int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Are");
 
-            if()
+            Console.Write("Are you currently enrolled? (yes/no): ");
+            string input1 = Console.ReadLine();
+            bool isEnrolled = true;
+            
+            Console.Write("Do you have a valid student ID? (yes/no): ");
+            string input2 = Console.ReadLine();
+            bool hasStudentId = true;
+            
+            if (age >= 18)
             {
-                
+                if (isEnrolled || hasStudentId) 
+                {
+                    Console.WriteLine("Campus Access Granted");
+                }
             }
 
+            Console.WriteLine("\nSECTION C - Semester Code Lookup");
+            Console.Write("Enter a semester code (SP, SU, FA): ");
+            string semesterCode = Console.ReadLine();
 
+            switch (semesterCode)
+            {
+                case "SP":
+                    Console.WriteLine("Spring");
+                    break;
+                case "SU":
+                    Console.WriteLine("Summer");
+                    break;
+                case "FA":
+                    Console.WriteLine("Fall");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
 
-            Console.WriteLine("SECTION C - Semester Code Lookup");
-
+            }
         }
     }
 }
